55bf3271cc36c6b332c2d47403112bbb4c6c3710
MilkTeaCat52 <91018175+MilkTeaCat52@users.noreply.github.com>
2024-10-14 20:04:56 +0800

    Implement FiveColor
    implemented FiveColor
    based Button Color functions on FiveColor

--- Helper/ColorHelper.cs
+++ Helper/ColorHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.UI.Xaml.Media;
+using System;
 using Windows.UI;
 
 namespace Timebook.Helper
@@ -36,60 +37,171 @@ namespace Timebook.Helper
             return ((byte)(0.2126 * R + 0.7152 * G + 0.0722 * B));
         }
 
+        private class FiveColor
+        {
+            public byte A;
+            public double L;
+            public byte R;
+            public byte G;
+            public byte B;
+
+            public FiveColor(byte A, byte R, byte G, byte B)
+            {
+                this.A = A;
+
+                double Max = Math.Max(Math.Max(R, G), B);
+                this.L = Max / 255;
+                this.R = (byte)(R / this.L);
+                this.B = (byte)(B / this.L);
+                this.G = (byte)(G / this.L);
+
+                if (this.R == 0 && this.G == 0 && this.B == 0)
+                {
+                    this.R = this.G = this.B = 255;
+                }
+            }
+
+            public FiveColor(Color color)
+            {
+                this.A = color.A;
+
+                var R = color.R;
+                var G = color.G;
+                var B = color.B;
+
+                double Max = Math.Max(Math.Max(R, G), B);
+                this.L = Max / 255;
+                this.R = (byte)(R / this.L);
+                this.B = (byte)(B / this.L);
+                this.G = (byte)(G / this.L);
+
+                if (this.R == 0 && this.G == 0 && this.B == 0)
+                {
+                    this.R = this.G = this.B = 255;
+                }
+
+            }
+
+            public static FiveColor FromBlend(Color frontColor, Color backColor)
+            {
+                double alphaPercentage = (double)frontColor.A / 255;
+                var R = (byte)(frontColor.R * alphaPercentage + backColor.R * (1 - alphaPercentage));
+                var G = (byte)(frontColor.G * alphaPercentage + backColor.G * (1 - alphaPercentage));
+                var B = (byte)(frontColor.B * alphaPercentage + backColor.B * (1 - alphaPercentage));
+
+                return new FiveColor(255, R, G, B);
+            }
+
+            public bool IsBright()
+            {
+                return L >= 0.5;
+            }
+
+            public void ShiftAlphaPositive(byte value)
+            {
+                A += value;
+            }
+
+            public void ShiftAlphaNegative(byte value)
+            {
+                A -= value;
+            }
+
+            public void ShiftBrightness(double value)
+            {
+                L += value / 100;
+            }
+
+            public void ScaleBrightness(double scale)
+            {
+                L *= scale;
+            }
+
+            public void FlipBrightness()
+            {
+                L = 1 - L;
+            }
+
+            public Color ToColor()
+            {
+                return Color.FromArgb(A, (byte)(R * L), (byte)(G * L), (byte)(B * L));
+            }
+        }
+
         public static Brush GetButtonHoverBrush(Color originalColor)
         {
-            double alphaPercentage = (double)originalColor.A / 255;
+            var fiveColor = new FiveColor(originalColor);
 
-            double backgroundHex = ThemeHelper.IsDarkTheme() ? 0x20 : 0xf9;
+            long backgroundHex = ThemeHelper.IsDarkTheme() ? 0xFF202020 : 0xFFf9f9f9;
+            var mixedFiveColor = FiveColor.FromBlend(originalColor,ColorHelper.HexToBrush(backgroundHex).Color);
 
-            byte mixedR = (byte)(originalColor.R * alphaPercentage + backgroundHex * (1 - alphaPercentage));
-            byte mixedG = (byte)(originalColor.G * alphaPercentage + backgroundHex * (1 - alphaPercentage));
-            byte mixedB = (byte)(originalColor.B * alphaPercentage + backgroundHex * (1 - alphaPercentage));
-
-
-            bool mixedBright = (mixedR > 128 || mixedG > 128 || mixedB > 128);
-
-            byte A, R, G, B;
-
-            if (mixedBright) //bright
+            if (mixedFiveColor.IsBright()) //bright
             {
-                A = 255;
-                R = (byte)(mixedR * 0.8);
-                G = (byte)(mixedG * 0.8);
-                B = (byte)(mixedB * 0.8);
-            }
-            else //dark
-            {
-                if (originalColor.A < 128) //handle transparency differently since the default button color is 0x0FFFFFFF
+                if (fiveColor.A < 128)
                 {
-                    A = (byte)((originalColor.A + 10) * 1.3);
-                    R = originalColor.R;
-                    G = originalColor.G;
-                    B = originalColor.B;
+                    if (fiveColor.IsBright())
+                    {
+                        fiveColor.FlipBrightness();
+                    }
+                    fiveColor.ShiftAlphaPositive(18);
                 }
                 else
                 {
-                    A = (byte)255;
-                    R = (byte)(255 - (255 - originalColor.R) * 0.88);
-                    G = (byte)(255 - (255 - originalColor.G) * 0.88);
-                    B = (byte)(255 - (255 - originalColor.B) * 0.88);
+                    fiveColor.ShiftBrightness(-20);
+                }
+            }
+            else //dark
+            {
+                if (fiveColor.A < 128) //handle transparency differently since the default button color is 0x0FFFFFFF
+                {
+                    if (!fiveColor.IsBright())
+                    {
+                        fiveColor.FlipBrightness();
+                    }
+                    fiveColor.ShiftAlphaPositive(18);
+                }
+                else
+                {
+                    if (ThemeHelper.IsDarkTheme())
+                    {
+                        double threshold = 10;
+
+                        if (fiveColor.L < threshold / 100)
+                        {
+                            fiveColor.L = 0.13;
+                        }
+                        else
+                        {
+                            fiveColor.ShiftBrightness(10);
+                        }
+                    }
+                    else
+                    {
+                        double threshold = 10;
+
+                        if (fiveColor.L < threshold / 100)
+                        {
+                            fiveColor.ShiftAlphaNegative(50);
+                        }
+                        else
+                        {
+                            fiveColor.ShiftAlphaNegative(40);
+                        }
+                    }
                 }
             }
 
-            return new SolidColorBrush(Color.FromArgb(A, R, G, B));
+
+            return new SolidColorBrush(fiveColor.ToColor());
         }
 
         public static Brush GetButtonPressedBrush(Color originalColor)
         {
-            byte A, R, G, B;
+            var fiveColor = new FiveColor(originalColor);
 
+            fiveColor.ScaleBrightness(0.4);
 
-            A = (byte)(originalColor.A);
-            R = (byte)(originalColor.R * 0.4);
-            G = (byte)(originalColor.G * 0.4);
-            B = (byte)(originalColor.B * 0.4);
-
-            return new SolidColorBrush(Color.FromArgb(A, R, G, B));
+            return new SolidColorBrush(fiveColor.ToColor());
         }
     }
 }
